297ca0cb633d6a6b7f21b0e80393c4d5cbbd1fec
Amal Benny <amalvelloor@outlook.com>
2021-12-14 06:48:14 +0530

    Update weight-calculator.py

--- weight-calc/weight-calculator.py
+++ weight-calc/weight-calculator.py
@@ -1,4 +1,4 @@
-print("Which one you need\n kg to pound (1),\n pound to kg(2)\n \"Type the associated number to required function\"")
+print("Which one you need\n kg to pound (1),\n pound to kg(2)\n Kilogram to Gram\n \"Type the associated number to required function\"")
 math= input()
 if (math=="1"): #kg to pound
     print(" Kilogram to Pound calc") #multiply the mass value by 2.205
@@ -7,7 +7,13 @@ if (math=="1"): #kg to pound
     print("\n In pound :")
     print(p_frkg)
 if(math=="2"):
+    print(" Pound to kg clac")
     p_kg = float(input(" Pound:"))
     kg_frp=p_kg/2.205
     print("\n in kg")
     print(kg_frp)
+if(math=="3"):
+    kg_g = float(input(" Kilogram: "))
+    g_frkg=kg_g*1000
+    print("\n In kg: ")
+    print(g_frkg)
